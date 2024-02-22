6fb941da076e4e491fe15b30d4ce9890ab9455c0
StiX1919 <devspencersmith@gmail.com>
2024-02-21 22:41:03 -0800

    Toying with basics

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import TestComponent from './components/component';
+
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Kick butts
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{count}</p>
+        <TestComponent num={count}/>
+        <button onClick={() => setCount(count)}>Click This to count</button>
       </header>
     </div>
   );
---
+++ src/components/component.js
@@ -0,0 +1,10 @@
+//WIP
+const TestComponent = props => {
+    return (
+      <div>
+        Times 2, {props.num * 2}
+      </div>
+    );
+  }
+
+export default TestComponent
